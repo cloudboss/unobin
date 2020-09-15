f255e945fd783b01a02fcdd8701226e272150708
Joseph Wright <rjosephwright@gmail.com>
2020-09-14 23:07:46 -0400

    Remove printing of error for CLI tool
    The spf13/cobra library prints errors, so having a fmt.Printf() causes
    duplicate error messages to be printed.

--- go-player/cmd/root.go
+++ go-player/cmd/root.go
@@ -20,7 +20,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -32,7 +31,6 @@ var RootCmd = &cobra.Command{
 
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
