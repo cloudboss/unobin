caebb9c7ca85412906f83eadde2cb29f24b557e5
Joseph Wright <rjosephwright@gmail.com>
2020-09-27 14:28:51 -0400

    Add a dummy Go file to root of project
    Running `pkger` fails unless there is a Go file in the root. See issues at
    https://github.com/markbates/pkger/issues/109 and
    https://github.com/markbates/pkger/issues/121.

---
+++ unobin.go
@@ -0,0 +1,4 @@
+// This file is here due to an issue with github.com/markbates/pkger,
+// see https://github.com/markbates/pkger/issues/109 and
+// https://github.com/markbates/pkger/issues/121.
+package unobin
